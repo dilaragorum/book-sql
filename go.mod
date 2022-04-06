11dd781af4cb10f3796db1f4aa390a10aa14a8c3
dilaragorum <dilaragorum@gmail.com>
2022-04-06 08:27:26 +0300

    GET/books implemented

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dilaragorum/book-sql
 
 go 1.17
 
-require github.com/lib/pq v1.10.4 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+)
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	_ "github.com/lib/pq"
 	"log"
+	"net/http"
 )
 
 // Golang <----DRIVER(pq)-----> DB PostgreSQL
@@ -16,36 +18,47 @@ type Book struct {
 	price  float32
 }
 
-func main() {
-	connectionPool, err := connectDB()
+// *sql.DB türünde connectionPool tanımlıyorum. Global tanımlamamın sebebi aşağıdaki örnekte init içerisinde
+// connectionPool'a atama yapıp, getBooks metodunda da bu connection'ını kitaplar DB'den getirmek için kullanıyorum.
+// We use the init() function to set up our connection pool and assign it to the global variable db.
+//We're using a global variable to store the connection pool
+//because it's an easy way of making it available to our HTTP handlers – but it's by no means the only way.
+var connectionPool *sql.DB
+
+func init() {
+	var err error
+	connectionPool, err = sql.Open("postgres", "postgres://postgres:postgres@localhost?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
+func main() {
+	router := httprouter.New()
+	router.GET("/books", GetBooks)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func GetBooks(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	books, err := GetAllBooksFromDB(connectionPool)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, "Error when getting all books from db", http.StatusInternalServerError)
+		return
 	}
 
 	for _, book := range books {
-		fmt.Println(book.price, book.title, book.isbn, book.author)
+		fmt.Fprintf(w, "%s %s %s %f\n", book.isbn, book.title, book.author, book.price)
 	}
 }
 
-func connectDB() (*sql.DB, error) {
-	connectionPool, err := sql.Open("postgres", "postgres://postgres:postgres@localhost?sslmode=disable")
-	if err != nil {
-		return nil, err
-	}
-	return connectionPool, nil
-}
-
+// Kitapları DB'den getirebilmek için yukarıdaki connection'u burada kullanmamız gerekiyor Query yaparken.
 func GetAllBooksFromDB(connectionPool *sql.DB) ([]Book, error) {
 	rows, err := connectionPool.Query("SELECT * FROM books")
 	if err != nil {
 		return []Book{}, err
 	}
-	defer rows.Close()
+	defer rows.Close() // açmış olduğum connection'ını işim bitince pool'a geri atıyorum.
 
 	books := make([]Book, 0)
 	for rows.Next() {
